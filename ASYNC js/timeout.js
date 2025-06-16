46dd6520d9240733dde2f81231b4938e046bfcdb
Aryan Raj Gupta <gupta.raj.aryan.2005@gmail.com>
2025-06-16 22:11:37 +0530

    Introduction too async  promises!!

--- ASYNC js/timeout.js
+++ ASYNC js/timeout.js
@@ -6,3 +6,6 @@ console.log("hi there !!");
 setTimeout(timeout, 12000);
 
 console.log("welcome to lounge !!");
+
+//Callback functions = these are function that calls another function after a certain task 
+// example over here a function is called after a certain time called the setTimeout function 
