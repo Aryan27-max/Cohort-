0b9e5950d111facb136cc887b8d78d52a3840039
Aryan Raj Gupta <gupta.raj.aryan.2005@gmail.com>
2025-07-06 11:38:39 +0530

    edditing callback !!

--- promises/CallbackHell.js
+++ promises/CallbackHell.js
@@ -3,14 +3,33 @@
 // prints hello after 3 seconds
 // prints hello there after 5 seconds 
 
-setTimeout(function (){
-    console.log("hi");
-    setTimeout(function (){
-    console.log("hello");
-    setTimeout(function (){
-    console.log("hello there");
-    }, 5000) // after 5 seconds this gets completed
-    }, 3000) // after 3 seconds this gets completed       // the code over here is very neseted with lot of callback thereby the name call back hell 
-}, 1000) // then after 1 sec this gets completed 
+// setTimeout(function (){
+//     console.log("hi");
+//     setTimeout(function (){
+//     console.log("hello");
+//     setTimeout(function (){
+//     console.log("hello there");
+//     }, 5000) // after 5 seconds this gets completed
+//     }, 3000) // after 3 seconds this gets completed       // the code over here is very neseted with lot of callback thereby the name call back hell 
+// }, 1000) // then after 1 sec this gets completed 
 
-console.log("out of calback hell"); // first thi swill execute as the code compiles
+// console.log("out of calback hell"); // first thi swill execute as the code compiles
+
+
+// alternate method to evade callback hll but always go with PROMISES 
+
+function step3Done(){
+    console.log("hello there");
+}
+
+function step2Done(){
+    console.log("hello");
+    setTimeout(step3Done,5000);
+}
+
+function step1Done(){
+    console.log("hi");
+    setTimeout(step2Done,3000);
+}
+setTimeout(step1Done,1000)
+
