5d266f0475de15f8d40dff14162ea311ccc9c5ed
Aryan Raj Gupta <gupta.raj.aryan.2005@gmail.com>
2025-07-06 11:24:50 +0530

    introduction to callback hell !!

---
+++ promises/CallbackHell.js
@@ -0,0 +1,16 @@
+// write a code (representation of call back hell)
+// prints hi after 1 second 
+// prints hello after 3 seconds
+// prints hello there after 5 seconds 
+
+setTimeout(function (){
+    console.log("hi");
+    setTimeout(function (){
+    console.log("hello");
+    setTimeout(function (){
+    console.log("hello there");
+    }, 5000) // after 5 seconds this gets completed
+    }, 3000) // after 3 seconds this gets completed       // the code over here is very neseted with lot of callback thereby the name call back hell 
+}, 1000) // then after 1 sec this gets completed 
+
+console.log("out of calback hell"); // first thi swill execute as the code compiles
