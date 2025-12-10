7f10c60540c3952932277901edf167dd196d066d
Aryan27-max <gupta.raj.aryan.2005@gmail.com>
2025-12-10 16:31:10 +0530

    JS learnings

--- Node-js/index.js
+++ Node-js/index.js
@@ -1,6 +1,6 @@
 const path = require("path");
 console.log(__dirname); // --> default node.js var that prints the directory names 
-console.log(path.join(__dirname, "index.js"));
+console.log(path.join(__dirname, "index.js")); // --> let's u join multiple paths 
 
 
 
