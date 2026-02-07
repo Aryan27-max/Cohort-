a0d9765b6a6a11aa540c81ac2fd59f486f1d63bc
Aryan27-max <gupta.raj.aryan.2005@gmail.com>
2026-02-07 15:53:57 +0530

    delete function

--- HTTP_servers/project/InMemoryHostpital.js
+++ HTTP_servers/project/InMemoryHostpital.js
@@ -10,16 +10,21 @@ const users = [{
 
 app.use(express.json());
 
+//GET kidneys stats
 app.get("/", function(req,res){
   const johnKidneys = users[0].kidneys;
   const numberOfKidneys = johnKidneys.length;
+
   let numberOfHealthyKidneys = 0;
+  
   for(let i=0; i < johnKidneys.length; i++){
     if (johnKidneys[i].healthy){
       numberOfHealthyKidneys = numberOfHealthyKidneys + 1;
     }
   }
+  
   const numberOfUnhealthyKidneys = numberOfKidneys - numberOfHealthyKidneys;
+  
   res.json({
     numberOfKidneys,
     numberOfHealthyKidneys,
@@ -42,122 +47,46 @@ app.put("/",function(req,res){
   for(let i=0 ; i<users[0].kidneys.length; i++){
     users[0].kidneys[i].healthy=true;
   }
+  res.json({});
 })
 
+//removing all the unhealthy kidneys 
+app.delete("/", function(req,res){
+
+  //only ifone unhealthy kidnet is there do this, elese return 411(wrong input)
+
+  if(isThereAtleastOneUnhealthyKidney){
+    
+  const newKidneys = [];
+  for(let i=0 ; i<users[0].kidneys.length; i++){
+    if (users[0].kidneys[i].healthy){  
+    newKidneys.push({
+        healthy:true
+      })
+    }
+  }
+  users[0].kidneys = newKidneys;
+  res.json({msg: "done"}) 
+  } 
+  else
+  {
+    res.status(411).json({
+      msg: "you have no bad kidneys" 
+    });
+  }
+})
+
+function isThereAtleastOneUnhealthyKidney(){
+  let atleastOneHelathyKidney = false;
+  for(let i=0 ; i<users[0].kidneys.length; i++){
+    if (users[0].kidneys[i].healthy){
+    atleastOneHelathyKidney = true;
+    }
+  }
+}
 app.listen(3000, () => {
   console.log("Server running at port 3000")
 })
 
 
-
-// td
-
-// const express = require("express");
-// const app = express();
-
-// app.use(express.json());
-
-// // In-memory database
-// const users = [
-//   {
-//     name: "john",
-//     kidneys: [
-//       { healthy: true },
-//       { healthy: true },
-//       { healthy: true },
-//       { healthy: false }
-//     ]
-//   }
-// ];
-
-// /**
-//  * GET /
-//  * Returns kidney statistics
-//  */
-// app.get("/", function (req, res) {
-//   const kidneys = users[0].kidneys;
-//   const numberOfKidneys = kidneys.length;
-
-//   let numberOfHealthyKidneys = 0;
-
-//   for (let i = 0; i < kidneys.length; i++) {
-//     if (kidneys[i].healthy === true) {
-//       numberOfHealthyKidneys++;
-//     }
-//   }
-
-//   const numberOfUnhealthyKidneys =
-//     numberOfKidneys - numberOfHealthyKidneys;
-
-//   res.json({
-//     numberOfKidneys,
-//     numberOfHealthyKidneys,
-//     numberOfUnhealthyKidneys
-//   });
-// });
-
-// /**
-//  * POST /
-//  * Adds a new kidney
-//  * Body: { "isHealthy": true | false }
-//  */
-// app.post("/", function (req, res) {
-//   const isHealthy = req.body.isHealthy;
-
-//   // Validation
-//   if (typeof isHealthy !== "boolean") {
-//     return res.status(400).json({
-//       error: "isHealthy must be a boolean"
-//     });
-//   }
-
-//   users[0].kidneys.push({
-//     healthy: isHealthy
-//   });
-
-//   res.json({
-//     msg: "Done!"
-//   });
-// });
-
-// /**
-//  * PUT /
-//  * Makes all kidneys healthy
-//  */
-// app.put("/", function (req, res) {
-//   const kidneys = users[0].kidneys;
-
-//   for (let i = 0; i < kidneys.length; i++) {
-//     kidneys[i].healthy = true;
-//   }
-
-//   res.json({
-//     msg: "All kidneys healed!"
-//   });
-// });
-
-// /**
-//  * DELETE /
-//  * Removes all unhealthy kidneys
-//  */
-// app.delete("/", function (req, res) {
-//   const kidneys = users[0].kidneys;
-//   const newKidneys = [];
-
-//   for (let i = 0; i < kidneys.length; i++) {
-//     if (kidneys[i].healthy === true) {
-//       newKidneys.push(kidneys[i]);
-//     }
-//   }
-
-//   users[0].kidneys = newKidneys;
-
-//   res.json({
-//     msg: "Unhealthy kidneys removed!"
-//   });
-// });
-
-// app.listen(3000, () => {
-//   console.log("Server running on port 3000");
-// });
-
+//server checks (to check the edge cases if the user sends some gibrish)
