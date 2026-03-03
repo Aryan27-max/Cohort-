b437cccb6f9293d16aaf7bb84b560394892b8ff8
Aryan27-max <gupta.raj.aryan.2005@gmail.com>
2026-03-03 07:19:30 +0530

    intro to middlewares

--- Middlewares/index.js
+++ Middlewares/index.js
@@ -1,1 +1,13 @@
-console.log("Welcome to middlewares & Cors"); 
+const express = require('express');
+
+const app = express();
+
+app.get('/ride1', function (req, res) {
+    res.json({
+        msg: "Welcome to ride 1"
+    });
+});
+
+app.listen(3000, () => {
+    console.log("Server running on http://localhost:3000");
+});
