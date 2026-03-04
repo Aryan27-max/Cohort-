8dcfeb2201cc6aa1484e4d6cc4758aad4b623411
Aryan27-max <gupta.raj.aryan.2005@gmail.com>
2026-03-04 22:44:01 +0530

    amusent middleware

--- Middlewares/index.js
+++ Middlewares/index.js
@@ -1,13 +1,30 @@
-const express = require('express');
-
+const express = require ("express");
 const app = express();
 
-app.get('/ride1', function (req, res) {
-    res.json({
-        msg: "Welcome to ride 1"
-    });
-});
+ function ticketChecker(req, res, next) {
+    const ticket = req.query.ticket;
+    if ( ticket == "free") {
+        next();
+    } else {
+        res.status(403).send("Acess Denied")
+    }
+ }
 
+
+ app.use(ticletChecker);
+
+ app.get("/ride1", function() {
+    res.send("You rode the first ride");
+ });
+
+ app.get("/ride2", function() {
+    res.send("You rode the second ride");
+ });
+
+ app.get("/ride3", function() {
+    res.send("You rode the third ride");
+ });
+ 
 app.listen(3000, () => {
-    console.log("Server running on http://localhost:3000");
+console.log("Server running on http://localhost3000");
 });
