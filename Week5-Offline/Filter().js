5a668ed00d1b9200eeb5325200b0e2fcd05e088d
Aryan27-max <gupta.raj.aryan.2005@gmail.com>
2026-05-26 01:39:52 +0530

    Add Filter() and Map() examples to demonstrate array methods in JavaScript

---
+++ Week5-Offline/Filter().js
@@ -0,0 +1,9 @@
+// filter() is inbuild in JS array 
+// filtyer() function is used when u only want the
+// elemenets that specify a condition 
+
+const nums = [1,2,3,4,5,6,7,8,9,10];
+
+const evenNums = nums.filter(nums => nums % 2 === 0);
+
+console.log(evenNums);
---
+++ Week5-Offline/Map().js
@@ -0,0 +1,9 @@
+// map() is inbuild in JS array 
+// map functon is used when u want to transform every element of an 
+// array to an new array 
+
+const nums = [1,2,3,4,5];
+
+const double = nums.map(nums => nums*2);
+
+console.log(double);
