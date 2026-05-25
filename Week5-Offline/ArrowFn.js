5d0ea1dac48a7209da38f06f09517cc7c07b1585
Aryan27-max <gupta.raj.aryan.2005@gmail.com>
2026-05-26 01:28:25 +0530

    Add ArrowFn.js to demonstrate arrow function syntax in JavaScript

---
+++ Week5-Offline/ArrowFn.js
@@ -0,0 +1,11 @@
+// arrow functions in js are shorter way to write functions in JS
+
+// function add(a,b){
+//     return a+b;
+// }
+
+// console.log(add(5,2))
+
+const add = (a, b) => a + b;
+
+console.log(add(5, 3)); // 8
