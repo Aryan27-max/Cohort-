5a6d6f01e4b12327b38913d35762ae9f3078122d
Aryan27-max <gupta.raj.aryan.2005@gmail.com>
2026-06-13 15:40:36 +0530

    Add index.html for user authentication interface

--- Week-6/FullJWT.js
+++ Week-6/FullJWT.js
@@ -1,80 +1,128 @@
-const express = require ('express');
-const jwt = require ('jsonwebtoken');
-const JWT_SECRET="wjkyhfgq4yuweatgfiweg"
+const express = require("express");
+const jwt = require("jsonwebtoken");
+
+const JWT_SECRET = "Aryan73642";
 
 const app = express();
 app.use(express.json());
 
 const users = [];
 
+// Logger Middleware
+function logger(req, res, next) {
+    console.log(req.method + " request came");
+    next();
+}
 
-app.post("/signup", function(req,res){
-    
+// Signup Route
+app.post("/signup", logger, function (req, res) {
     const username = req.body.username;
     const password = req.body.password;
 
-    users.push ({
-        username: username,
-        password: password
-    })
+    // Check if user already exists
+    const existingUser = users.find(
+        user => user.username === username
+    );
 
-    res.json({
-        message: "You are signed up"
-    })
-    console.log(users);
-})
+    if (existingUser) {
+        return res.status(409).json({
+            message: "User already exists"
+        });
+    }
 
-app.post("/signin", function (req,res){
-    
+    users.push({
+        username,
+        password
+    });
+
+    res.status(201).json({
+        message: "User signed up successfully"
+    });
+});
+
+// Signin Route
+app.post("/signin", logger, function (req, res) {
     const username = req.body.username;
     const password = req.body.password;
 
-    //maps and filters 
-    let FoundUsers = null;
+    let foundUser = null;
 
-    for (let i =0 ; i < users.length; i++){
-        if(users[i].username == username && users[i].password == password){
-            FoundUsers = users[i] 
+    for (let i = 0; i < users.length; i++) {
+        if (
+            users[i].username === username &&
+            users[i].password === password
+        ) {
+            foundUser = users[i];
+            break;
         }
     }
 
-    if(FoundUsers){
-        const token = jwt.sign({
-            username: username
-        }, JWT_SECRET);
-        
-        // FoundUsers.token = token; No need to stores as JWT is stateless
-        
-        res.json({
-            token: token
-        })
-    }else{
-        res.status(403).send({
-            message: "Invalid username or password"
-        })
-    }
-    console.log(users)
-})
-
-//creating an authenticated endpoint
-app.get("/me", function(req,res){
-
-    try{
-        const token = req.headers.token;
-
-        const decodedInformation = jwt.verify(token, JWT_SECRET);
-
-        res.json({
-            username: decodedInformation.username
+    if (!foundUser) {
+        return res.status(401).json({
+            message: "Invalid credentials"
         });
+    }
 
-    }catch(err){
-        res.status(403).json({
+    const token = jwt.sign(
+        {
+            username: foundUser.username
+        },
+        JWT_SECRET
+    );
+
+    res.json({
+        token: token
+    });
+});
+
+// Authentication Middleware
+function auth(req, res, next) {
+    const token = req.headers.token;
+
+    if (!token) {
+        return res.status(401).json({
+            message: "Token missing"
+        });
+    }
+
+    try {
+        const decodedData = jwt.verify(token, JWT_SECRET);
+
+        req.username = decodedData.username;
+
+        next();
+    } catch (err) {
+        return res.status(401).json({
             message: "Invalid token"
         });
     }
+}
+
+// Protected Route
+app.get("/me", logger, auth, function (req, res) {
+    const currentUser = req.username;
+
+    let foundUser = null;
+
+    for (let i = 0; i < users.length; i++) {
+        if (users[i].username === currentUser) {
+            foundUser = users[i];
+            break;
+        }
+    }
+
+    if (!foundUser) {
+        return res.status(404).json({
+            message: "User not found"
+        });
+    }
+
+    res.json({
+        username: foundUser.username,
+        password: foundUser.password
+    });
 });
 
 app.listen(3000, () => {
-    console.log("server running at port 3000");
+    console.log("Server running on port 3000");
 });
