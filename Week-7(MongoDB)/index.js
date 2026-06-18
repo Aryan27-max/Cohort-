936a3bfcaa1a90ee09e582f510d67ca32a0d8b0b
Aryan27-max <gupta.raj.aryan.2005@gmail.com>
2026-06-19 00:59:53 +0530

    Refactor authentication logic and update signup/signin endpoints for improved functionality

--- Week-7(MongoDB)/index.js
+++ Week-7(MongoDB)/index.js
@@ -1,105 +1,83 @@
 const express = require("express");
 const { UserModel, TodoModel } = require("./db");
+const { auth, JWT_SECRET } = require("./auth");
 const jwt = require("jsonwebtoken");
-const JWT_SECRET = "asgrfaAryan@1213251";
 const mongoose = require("mongoose");
-// const dns = require("dns");
 
-// dns.setServers(["8.8.8.8", "8.8.4.4"]);
-
-mongoose.connect(
-  "mongodb+srv://Aryan2222:wXtwdvwOLnwOzyjq@cluster0.7n3ce9a.mongodb.net/ "
-)
-.then(() => console.log("MongoDB Connected"))
-.catch(err => console.log(err));
+mongoose.connect("")
 
 const app = express();
 app.use(express.json());
 
-app.post("/signup", async function (req, res) {
-  const email = req.body.email;
-  const password = req.body.password;
-  const name = req.body.name;
+app.post("/signup", async function(req, res) {
+    const email = req.body.email;
+    const password = req.body.password;
+    const name = req.body.name;
 
-  await UserModel.create({
-    email: email,
-    password: password,
-    name: name,
-  });
-
-  res.json({
-    message: "User created successfully",
-  });
+    await UserModel.create({
+        email: email,
+        password: password,
+        name: name
+    });
+    
+    res.json({
+        message: "You are signed up"
+    })
 });
 
-app.post("/signin", async function (req, res) {
-  const email = req.body.email;
-  const password = req.body.password;
 
-  const user = await UserModel.findOne({
-    email: email,
-    password: password,
-  });
+app.post("/signin", async function(req, res) {
+    const email = req.body.email;
+    const password = req.body.password;
 
-  if (user) {
-    const token = jwt.sign(
-      {
-        id: user._id.toString() ,
-      },
-      JWT_SECRET
-    );
+    const response = await UserModel.findOne({
+        email: email,
+        password: password,
+    });
+
+    if (response) {
+        const token = jwt.sign({
+            id: response._id.toString()
+        }, JWT_SECRET);
+
+        res.json({
+            token
+        })
+    } else {
+        res.status(403).json({
+            message: "Incorrect creds"
+        })
+    }
+});
+
+
+app.post("/todo", auth, async function(req, res) {
+    const userId = req.userId;
+    const title = req.body.title;
+    const done = req.body.done;
+
+    await TodoModel.create({
+        userId,
+        title,
+        done
+    });
 
     res.json({
-      token: token,
-    });
-  } else {
-    res.status(403).json({
-      message: "Incorrect credentials",
-    });
-  }
-});
-
-app.post("/todos", auth, function (req, res) {
-  const userId = req.userId;
-  const title = req.body.title;
-
-  TodoModel.create({
-    title,
-    userId
-  })
-
-  res.json({
-    userId: userId
-  })
-
-});
-
-app.get("/todos", auth, async function (req, res) {
-  const userId = req.userId;
-  const todos = await TodoModel.find({
-    userId: userId
-  })
-
-  res.json({
-    todos
-  })
-
-});
-
-function auth(req,res,next){
-  const token = req.headers.token;
-
-  const decodedData = jwt.verify(token, JWT_SECRET);
-  
-  if (decodedData){
-    req.userId = decodedData.id;
-    next();
-  }
-  else {
-    res.status(403).json({
-      message: "incorrect credentials"
+        message: "Todo created"
     })
-  }
-}
+});
 
-app.listen(3000, () => console.log("Listening at port 3000"));
+
+app.get("/todos", auth, async function(req, res) {
+    const userId = req.userId;
+
+    const todos = await TodoModel.find({
+        userId
+    });
+
+    res.json({
+        todos
+    })
+});
+
+app.listen(3000);
