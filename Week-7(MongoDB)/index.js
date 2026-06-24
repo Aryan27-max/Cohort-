c339f0fd2516c91dba69ea38825bedc210f4a326
Aryan27-max <gupta.raj.aryan.2005@gmail.com>
2026-06-24 19:17:37 +0530

    Add dns dependency to package.json for DNS resolution functionality

--- Week-7(MongoDB)/index.js
+++ Week-7(MongoDB)/index.js
@@ -1,83 +1,107 @@
 const express = require("express");
-const { UserModel, TodoModel } = require("./db");
-const { auth, JWT_SECRET } = require("./auth");
+const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const mongoose = require("mongoose");
 
-mongoose.connect("")
+const { UserModel, TodoModel } = require("./db");
+const { auth, JWT_SECRET } = require("./auth");
+
+// Optional DNS fix for MongoDB Atlas
+const dns = require("dns");
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
+
+mongoose.connect(
+  "mongodb+srv://Aryan2222:wXtwdvwOLnwOzyjq@cluster0.7n3ce9a.mongodb.net/aryan-todo"
+);
 
 const app = express();
+
 app.use(express.json());
 
-app.post("/signup", async function(req, res) {
-    const email = req.body.email;
-    const password = req.body.password;
-    const name = req.body.name;
+app.post("/signup", async function (req, res) {
+  const email = req.body.email;
+  const password = req.body.password;
+  const name = req.body.name;
 
-    await UserModel.create({
-        email: email,
-        password: password,
-        name: name
-    });
-    
-    res.json({
-        message: "You are signed up"
+  // Hash password before storing
+  const hashedPassword = await bcrypt.hash(password, 5);
+
+  console.log(hashedPassword);
+
+  await UserModel.create({
+    email: email,
+    password: hashedPassword,
+    name: name,
+  });
+
+  res.json({
+    message: "You are signed up",
+  });
+});
+
+app.post("/signin", async function (req, res) {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  const response = await UserModel.findOne({
+    email: email,
+  });
+
+  if(!response){
+    res.status(403).json({
+        message:"User does not exist"
     })
-});
+    return
+  }
 
+  const passwordMatch = bcrypt.compare(password, response.password);
 
-app.post("/signin", async function(req, res) {
-    const email = req.body.email;
-    const password = req.body.password;
-
-    const response = await UserModel.findOne({
-        email: email,
-        password: password,
-    });
-
-    if (response) {
-        const token = jwt.sign({
-            id: response._id.toString()
-        }, JWT_SECRET);
-
-        res.json({
-            token
-        })
-    } else {
-        res.status(403).json({
-            message: "Incorrect creds"
-        })
-    }
-});
-
-
-app.post("/todo", auth, async function(req, res) {
-    const userId = req.userId;
-    const title = req.body.title;
-    const done = req.body.done;
-
-    await TodoModel.create({
-        userId,
-        title,
-        done
-    });
+  if (passwordMatch) {
+    const token = jwt.sign(
+      {
+        id: response._id.toString(),
+      },
+      JWT_SECRET
+    );
 
     res.json({
-        message: "Todo created"
-    })
-});
-
-
-app.get("/todos", auth, async function(req, res) {
-    const userId = req.userId;
-
-    const todos = await TodoModel.find({
-        userId
+      token,
     });
-
-    res.json({
-        todos
-    })
+  } else {
+    res.status(403).json({
+      message: "Incorrect creds",
+    });
+  }
 });
 
-app.listen(3000);
+app.post("/todo", auth, async function (req, res) {
+  const userId = req.userId;
+  const title = req.body.title;
+  const done = req.body.done;
+
+  await TodoModel.create({
+    userId,
+    title,
+    done,
+  });
+
+  res.json({
+    message: "Todo created",
+  });
+});
+
+app.get("/todos", auth, async function (req, res) {
+  const userId = req.userId;
+
+  const todos = await TodoModel.find({
+    userId,
+  });
+
+  res.json({
+    todos,
+  });
+});
+
+app.listen(3000, () => {
+  console.log("Server running at port 3000");
+});
