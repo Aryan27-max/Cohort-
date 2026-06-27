c7e8b4499ad16a696f6ae6c435c1797e1795696a
Aryan27-max <gupta.raj.aryan.2005@gmail.com>
2026-06-28 05:12:59 +0530

    feat: add db.js file with Mongoose schemas for user, admin, course, and purchase

---
+++ Course_selling_Week-8/db.js
@@ -0,0 +1,51 @@
+const mongoose = require ("mongoose");
+const Schema = mongoose.Schema;
+const ObjectId = mongoose.Schema.Types.ObjectId;
+
+const userSchema = new Schema({
+        email: {type: String, unique: true, required: true},
+        password: String, 
+        FirstName: String,
+        LastName: String
+});
+
+const adminSchema = new Schema({
+        email: {type: String, unique: true, required: true},
+        password: String, 
+        FirstName: String,
+        LastName: String
+});
+
+const courseSchema = new Schema({
+        title: String,
+        description: String,
+        price: Number,
+        imageURL: String,
+        creatorID: String
+
+});
+
+ 
+const purchaseSchema = new Schema({
+        userId: {
+            type: Schema.Types.ObjectId,
+            ref:"user"
+        },
+        courseId: {
+            type: Schema.Types.ObjectId,
+            ref:"course"
+        },
+});
+
+const UserModel = mongoose.model("user", userSchema);
+const AdminModel = mongoose.model("admin", adminSchema);
+const CourseModel = mongoose.model("course", courseSchema);
+const PurchaseModel = mongoose.model("purchase", purchaseSchema);
+
+module.exports = {
+  UserModel,
+  AdminModel,
+  CourseModel,
+  PurchaseModel
+};
+
