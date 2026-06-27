9cb152b00d2e2959de690a650987803d70870549
Aryan27-max <gupta.raj.aryan.2005@gmail.com>
2026-06-27 17:54:10 +0530

    feat: add admin routes and update index.js for routing

---
+++ Course_selling_Week-8/routes/admin.js
@@ -0,0 +1,36 @@
+const { Router } = require ("express");
+const adminRouter = Router();
+
+adminRouter.post("/signup", function(req,res){
+    res.json({
+        message: "Signup endpoint"
+    })
+})
+
+adminRouter.post("/signin", function(req,res){
+    res.json({
+        message: "Signin endpoint"
+    })
+})
+
+adminRouter.post("/courses", function(req,res){
+    res.json({
+        message: "courses endpoint"
+    })
+})
+
+adminRouter.put("/courses", function(req,res){
+    res.json({
+        message: "courses endpoint"
+    })
+})
+
+adminRouter.get("/courses/bulk", function(req,res){
+    res.json({
+        message: "courses endpoint"
+    })
+})
+
+module.exports = {
+    adminRouter: adminRouter
+}
