501179251ac08e5377bcfd8f4051ad90458a8656
Aryan27-max <gupta.raj.aryan.2005@gmail.com>
2026-07-04 13:15:30 +0530

    feat: seprated auth pathways fro user and admin

--- Course_selling_Week-8/middlewares/auth.js
+++ Course_selling_Week-8/middlewares/auth.js
@@ -1,23 +1,57 @@
-const jwt = require ("jsonwebtoken");
-const JWT_SECRET = process.env.JWT_SECRET
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
 
-function auth(req,res, next){
+const JWT_USER_PASSWORD = process.env.JWT_USER_PASSWORD;
+const JWT_ADMIN_PASSWORD = process.env.JWT_ADMIN_PASSWORD;
+
+
+// user middleware
+function userAuth(req,res,next){
     try{
         const token = req.headers.authorization;
 
-        const response = jwt.verify(token, JWT_SECRET);
+        const response = jwt.verify(
+            token,
+            JWT_USER_PASSWORD
+        );
 
         req.userID = response.id;
+
         next();
     }
     catch(err){
         res.status(403).json({
-            message:"Incorrect creds"
-        })
+            message:"Incorrect user creds"
+        });
     }
 }
 
+
+// admin middleware
+function adminAuth(req,res,next){
+    try{
+        const token = req.headers.authorization;
+
+        const response = jwt.verify(
+            token,
+            JWT_ADMIN_PASSWORD
+        );
+
+        req.adminID = response.id;
+
+        next();
+    }
+    catch(err){
+        res.status(403).json({
+            message:"Incorrect admin creds"
+        });
+    }
+}
+
+
 module.exports = {
-    auth,
-    JWT_SECRET
+    userAuth,
+    adminAuth,
+    JWT_USER_PASSWORD,
+    JWT_ADMIN_PASSWORD
 }
