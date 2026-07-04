a9f07695af7cfbc092761a8448f925ebb5408407
Aryan27-max <gupta.raj.aryan.2005@gmail.com>
2026-07-04 10:55:40 +0530

    feat: signup endpoint users fully functioning

--- Course_selling_Week-8/routes/user.js
+++ Course_selling_Week-8/routes/user.js
@@ -1,12 +1,62 @@
 const { Router } = require ("express");
+const { z } = require("zod");
+const { UserModel } = require("../db");
+const userRouter = Router ();
+const { auth, JWT_SECRET } = require ("../middlewares/auth.js");
+const bcrypt = require("bcrypt")
 
-const userRouter = Router();
 
-userRouter.post("/signup", function(req,res){
-    res.json({
-        message: "Signup endpoint"
-    })
-})
+userRouter.post("/signup", async function(req,res){
+
+    try{
+        const requiredBody = z.object({
+            email: z.string().min(3).max(30).email(),
+            password: z.string().min(3).max(100),
+            FirstName: z.string().min(3).max(100),
+            LastName: z.string().min(3).max(100),
+        })
+
+        const parsedDataWithSucess = requiredBody.safeParse(req.body);
+        //over here .safeParse() is a zod function that finds if the req.body follows the schema defined above 
+
+        if(!parsedDataWithSucess.success){
+            res.status(400).json({
+                message: "Incorrect Password",
+                error: parsedDataWithSucess.error
+            })
+            return
+        }
+
+        const { email, password, FirstName, LastName } = parsedDataWithSucess.data;
+
+        const existingUser = await UserModel.findOne({ email })
+
+        if(existingUser){
+            return res.status(409).json({
+                message: "user already exists"
+            });
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        await UserModel.create({
+            email: email,
+            password: hashedPassword, 
+            FirstName: FirstName,
+            LastName: LastName
+         })
+
+         res.json({
+            message: "you are signed up"
+         })
+    }
+    catch(err){
+            res.status(500).json({
+                message: "Internal Server error",
+                error: err.message
+            })
+        }
+})   
 
 userRouter.post("/signin", function(req,res){
     res.json({
