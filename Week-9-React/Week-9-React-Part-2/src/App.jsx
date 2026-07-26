c42f0a3fbdec73d4d5d6bad67f5e643f1528dc69
Aryan27-max <gupta.raj.aryan.2005@gmail.com>
2026-07-27 00:51:01 +0530

    basic counter code

--- Week-9-React/Week-9-React-Part-2/src/App.jsx
+++ Week-9-React/Week-9-React-Part-2/src/App.jsx
@@ -1,11 +1,26 @@
+import { useState } from "react"
+
 function App() {
     return <div>
       <b>
         hi there 
       </b>
+      <Counter></Counter>
     </div>
 }
 
-function
+function Counter () {
+
+  const [count, setCount] = useState(0);
+
+  function increaseCount(){
+    setCount(count + 1);
+  }
+
+  return <div>
+    <h1 id="text">{count}</h1>
+    <button onClick={increaseCount}>Increase count</button>
+  </div>
+}
 
 export default App
