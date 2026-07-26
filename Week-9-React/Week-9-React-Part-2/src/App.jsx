562719afc1a0a43ae11170cb5c611a50dbc38eb8
Aryan27-max <gupta.raj.aryan.2005@gmail.com>
2026-07-27 00:56:58 +0530

    basic counter application

--- Week-9-React/Week-9-React-Part-2/src/App.jsx
+++ Week-9-React/Week-9-React-Part-2/src/App.jsx
@@ -17,9 +17,19 @@ function Counter () {
     setCount(count + 1);
   }
 
+  function DecreaseCount(){
+    setCount(count - 1);
+  }
+
+  function ResetCount(){
+    setCount(0);
+  }
+
   return <div>
     <h1 id="text">{count}</h1>
     <button onClick={increaseCount}>Increase count</button>
+    <button onClick={DecreaseCount}>Decrease count</button>
+    <button onClick={ResetCount}>Reset count</button>
   </div>
 }
 
